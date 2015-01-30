01e53b574e0b4d2712a9637e4f5feeb3f72bc4f5
gpbenton <gpbenton@live.com>
2015-01-30 14:19:11 +0000

    Store session data complete.

--- PasswordMaker/js/default.js
+++ PasswordMaker/js/default.js
@@ -6,19 +6,99 @@
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
 
+    app.on
+
     app.onactivated = function (args) {
+
         if (args.detail.kind === activation.ActivationKind.launch) {
+
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
+                var storedpreURL = WinJS.Application.sessionState.preURL;
+                if (storedpreURL) {
+                    preURL.innerText = storedpreURL;
+                }
+
+                var storedwhereLeetLB = WinJS.Application.sessionState.whereLeetLB;
+                if (storedwhereLeetLB) {
+                    whereLeetLB.value = storedwhereLeetLB;
+                }
+
+                var storedleetLevelLB = WinJS.Application.sessionState.leetLevelLB;
+                if (storedleetLevelLB) {
+                    leetLevelLB.value = storedleetLevelLB;
+                }
+
+                var storedhashAlgorithmLB = WinJS.Application.sessionState.hashAlgorithmLB;
+                if (storedhashAlgorithmLB) {
+                    hashAlgorithmLB.value = storedhashAlgorithmLB;
+                }
+
+                var storedprotocolCB = WinJS.Application.sessionState.protocolCB;
+                if (storedprotocolCB !== undefined) {
+                    protocolCB.status = storedprotocolCB;
+                }
+
+                var storedsubdomainCB = WinJS.Application.sessionState.subdomainCB;
+                if (storedsubdomainCB !== undefined) {
+                    subdomainCB.status = storedsubdomainCB;
+                }
+
+                var storeddomainCB = WinJS.Application.sessionState.domainCB;
+                if (storeddomainCB !== undefined) {
+                    domainCB.status = storeddomainCB;
+                }
+
+                var storedpathCB = WinJS.Application.sessionState.pathCB;
+                if (storedpathCB !== undefined) {
+                    pathCB.status = storedpathCB;
+                }
+
+                var storedpasswdUrl = WinJS.Application.sessionState.passwdUrl;
+                if (storedpasswdUrl) {
+                    passwdUrl.innerText = storedpasswdUrl;
+                }
+
+                if (WinJS.Application.sessionState.passwdLength) {
+                    passwdLength.value = WinJS.Application.sessionState.passwdLength;
+                }
+
+                if (WinJS.Application.sessionState.usernameTB) {
+                    usernameTB.innerText = WinJS.Application.sessionState.usernameTB;
+                }
+
+                if (WinJS.Application.sessionState.counter) {
+                    counter.innerText = WinJS.Application.sessionState.counter;
+                }
+
+                if (WinJS.Application.sessionState.charset) {
+                    charset.selectedIndex = WinJS.Application.sessionState.charset;
+                }
+
+                if (WinJS.Application.sessionState.passwordPrefix) {
+                    passwordPrefix.innerText = WinJS.Application.sessionState.passwordPrefix;
+                }
+
+                if (WinJS.Application.sessionState.passwordSuffix) {
+                    passwordSuffix.innerText = WinJS.Application.sessionState.passwordSuffix;
+                }
+
+                if (WinJS.Application.sessionState.ifHidePasswd !== undefined) {
+                    ifHidePasswd.status = WinJS.Application.sessionState.ifHidePasswd;
+                }
+
+                preGeneratePassword();
+
             }
+
             args.setPromise(WinJS.UI.processAll().then(
                 function completed() {
 
-                    preURL.addEventListener("change", populateURL, false);
+                    preURL.addEventListener("change", preURLHandler, false);
                     preURL.addEventListener("keyup", populateURL, false);
                     preURL.addEventListener("keydown", populateURL, false);
                     preURL.addEventListener("keypress", populateURL, false);
@@ -26,7 +106,7 @@
 
                     copyFromClipboard.addEventListener("click", copyFromClipboardHandler, false);
 
-                    passwdMaster.addEventListener("change", preGeneratePassword, false);
+                    passwdMaster.addEventListener("change", passwdMasterHandler, false);
                     passwdMaster.addEventListener("keyup", preGeneratePassword, false);
                     passwdMaster.addEventListener("keydown", preGeneratePassword, false);
                     passwdMaster.addEventListener("keypress", preGeneratePassword, false);
@@ -36,20 +116,20 @@
 
                     whereLeetLB.addEventListener("change", whereLeetLBHandler, false);
 
-                    leetLevelLB.addEventListener("change", preGeneratePassword, false);
+                    leetLevelLB.addEventListener("change", leetLevelLBHandler, false);
 
-                    hashAlgorithmLB.addEventListener("change", preGeneratePassword, false);
+                    hashAlgorithmLB.addEventListener("change", hashAlgorithmLBHandler, false);
                     hashAlgorithmLB.addEventListener("keyup", preGeneratePassword, false);
                     hashAlgorithmLB.addEventListener("keydown", preGeneratePassword, false);
                     hashAlgorithmLB.addEventListener("keypress", preGeneratePassword, false);
                     hashAlgorithmLB.addEventListener("input", preGeneratePassword, false);
 
-                    protocolCB.addEventListener("click", populateURL, false);
-                    subdomainCB.addEventListener("click", populateURL, false);
-                    domainCB.addEventListener("click", populateURL, false);
-                    pathCB.addEventListener("click", populateURL, false);
+                    protocolCB.addEventListener("click", protocolCBHandler, false);
+                    subdomainCB.addEventListener("click", subdomainCBHandler, false);
+                    domainCB.addEventListener("click", domainCBHandler, false);
+                    pathCB.addEventListener("click", pathCBHandler, false);
 
-                    passwdUrl.addEventListener("change", preGeneratePassword, false);
+                    passwdUrl.addEventListener("change", passwdUrlHandler, false);
                     passwdUrl.addEventListener("keyup", preGeneratePassword, false);
                     passwdUrl.addEventListener("keydown", preGeneratePassword, false);
                     passwdUrl.addEventListener("keypress", preGeneratePassword, false);
@@ -61,27 +141,27 @@
                     passwdLength.addEventListener("keypress", passwdLengthHandler, false);
                     passwdLength.addEventListener("input", passwdLengthHandler, false);
 
-                    usernameTB.addEventListener("change", preGeneratePassword, false);
+                    usernameTB.addEventListener("change", usernameTBHandler, false);
                     usernameTB.addEventListener("keyup", preGeneratePassword, false);
                     usernameTB.addEventListener("keydown", preGeneratePassword, false);
                     usernameTB.addEventListener("keypress", preGeneratePassword, false);
                     usernameTB.addEventListener("input", preGeneratePassword, false);
 
-                    counter.addEventListener("change", preGeneratePassword, false);
+                    counter.addEventListener("change", counterHandler, false);
                     counter.addEventListener("keyup", preGeneratePassword, false);
                     counter.addEventListener("keydown", preGeneratePassword, false);
                     counter.addEventListener("keypress", preGeneratePassword, false);
                     counter.addEventListener("input", preGeneratePassword, false);
 
-                    charset.addEventListener("change", preGeneratePassword, false);
+                    charset.addEventListener("change", charsetHandler, false);
 
-                    passwordPrefix.addEventListener("change", preGeneratePassword, false);
+                    passwordPrefix.addEventListener("change", passwordPrefixHandler, false);
                     passwordPrefix.addEventListener("keyup", preGeneratePassword, false);
                     passwordPrefix.addEventListener("keydown", preGeneratePassword, false);
                     passwordPrefix.addEventListener("keypress", preGeneratePassword, false);
                     passwordPrefix.addEventListener("input", preGeneratePassword, false);
 
-                    passwordSuffix.addEventListener("change", preGeneratePassword, false);
+                    passwordSuffix.addEventListener("change", passwordSuffixHandler, false);
                     passwordSuffix.addEventListener("keyup", preGeneratePassword, false);
                     passwordSuffix.addEventListener("keydown", preGeneratePassword, false);
                     passwordSuffix.addEventListener("keypress", preGeneratePassword, false);
@@ -107,11 +187,15 @@
     function whereLeetLBHandler(eventInfo) {
         onWhereLeetLBChanged();
         preGeneratePassword();
+
+        WinJS.Application.sessionState.whereLeetLB = whereLeetLB.value;
     }
 
     function passwdLengthHandler(eventInfo) {
         if (/\D/.test(this.value)) this.value = '8';
         preGeneratePassword();
+
+        WinJS.Application.sessionState.passwdLength = passwdLength.value;
     }
 
     function ifHidePasswdHandler(eventInfo) {
@@ -120,7 +204,8 @@
         } else {
             passwdGenerated.style.color = '#00f';
         }
-        saveGlobalPrefs();
+        
+        WinJS.Application.sessionState.ifHidePasswd = ifHidePasswd.status;
     }
 
     function copyFromClipboardHandler(eventInfo) {
@@ -128,6 +213,11 @@
         if (dataPackageView.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
             dataPackageView.getTextAsync().then(function (text) {
                 preURL.innerText = text;
+
+                populateURL();
+                WinJS.Application.sessionState.preURL = preUrl.value;
+                WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
             });
         }
     }
@@ -137,6 +227,101 @@
         var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
         dataPackage.setText(passwdGenerated.value);
         Windows.ApplicationModel.DataTransfer.Clipboard.setContent(dataPackage);
+    }
+
+    function passwdMasterHandler(eventInfo) {
+        preGeneratePassword();
+    }
+        
+    function preURLHandler(eventInfo) {
+        populateURL();
+
+        WinJS.Application.sessionState.preURL = preURL.value;
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+    }
+
+    function leetLevelLBHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.leetLevelLB = leetLevelLB.value;
+    }
+
+    function hashAlgorithmLBHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.hashAlgorithmLB = hashAlgorithmLB.value;
+
+    }
+
+    function protocolCBHandler(eventInfo) {
+        populateURL();
+
+        WinJS.Application.sessionState.protocolCB = protocolCB.status;
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+    }
+
+    function subdomainCBHandler(eventInfo) {
+        populateURL();
+
+        WinJS.Application.sessionState.subdomainCB = subdomainCB.status;
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+    }
+
+    function domainCBHandler(eventInfo) {
+        populateURL();
+
+        WinJS.Application.sessionState.domainCB = domainCB.status;
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+    }
+
+    function pathCBHandler(eventInfo) {
+        populateURL();
+
+        WinJS.Application.sessionState.pathCB = pathCB.status;
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+    }
+
+    function passwdUrlHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+    }
+
+    function usernameTBHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.usernameTB = usernameTB.value;
+    }
+
+    function counterHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.counter = counter.value;
+    }
+
+    function charsetHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.charset = charset.selectedIndex;
+
+    }
+
+    function passwordPrefixHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.passwordPrefix = passwordPrefix.value;
+
+    }
+
+    function passwordSuffixHandler(eventInfo) {
+        preGeneratePassword();
+
+        WinJS.Application.sessionState.passwordSuffix = passwordSuffix.value;
     }
 
     app.start();
