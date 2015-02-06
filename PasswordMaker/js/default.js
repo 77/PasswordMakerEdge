ac4336ba50fe5b89828174e36f4c42969f2865c0
gpbenton <gpbenton@live.com>
2015-02-06 18:54:18 +0000

    Sort out fly-out pages - Help, about and licenses

--- PasswordMaker/js/default.js
+++ PasswordMaker/js/default.js
@@ -1,5 +1,16 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
+﻿// Copyright 2015 Graham Benton
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (function () {
     "use strict";
 
@@ -172,7 +183,10 @@
                     // Populate Settings pane and tie commands to Settings flyouts.
                     WinJS.Application.onsettings = function (e) {
                         e.detail.applicationcommands = {
-                            "helpDiv": { href: "HelpUI.html", title: "Help" }
+                            "helpDiv": { href: "HelpUI.html", title: "Help" },
+                            "licenseDiv": { href: "license.html", title: "License" },
+                            "pwmLibLicenseDiv": { href: "pwmLibLicense.html", title: "Library Licenses" },
+                            "aboutDiv": {href: "about.html", title: "About"}
                         };
                         WinJS.UI.SettingsFlyout.populateSettings(e);
                     }
