cce503d5bb9da9349da7235b8294faf10d0788ef
gpbenton <gpbenton@live.com>
2015-02-09 18:17:02 +0000

    PasswordMaker acts as share target for text and weblink

--- PasswordMaker/js/default.js
+++ PasswordMaker/js/default.js
@@ -1,21 +1,35 @@
 ﻿// Copyright 2015 Graham Benton
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//Microsoft Public License (MS-PL)
+
+//This license governs use of the accompanying software. If you use the software, you
+//accept this license. If you do not accept the license, do not use the software.
+
+//1. Definitions
+//The terms "reproduce," "reproduction," "derivative works," and "distribution" have the
+//same meaning here as under U.S. copyright law.
+//A "contribution" is the original software, or any additions or changes to the software.
+//A "contributor" is any person that distributes its contribution under this license.
+//"Licensed patents" are a contributor's patent claims that read directly on its contribution.
+
+//2. Grant of Rights
+//(A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+//(B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+
+//3. Conditions and Limitations
+//(A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+//(B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+//(C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+//(D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+//(E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+
 (function () {
     "use strict";
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var shareOperation = null;
 
     app.onactivated = function (args) {
 
@@ -102,98 +116,111 @@
 
                 preGeneratePassword();
 
-            }
+            } 
 
             args.setPromise(WinJS.UI.processAll().then(
-                function completed() {
-
-                    preURL.addEventListener("change", preURLHandler, false);
-                    preURL.addEventListener("keyup", populateURL, false);
-                    preURL.addEventListener("keydown", populateURL, false);
-                    preURL.addEventListener("keypress", populateURL, false);
-                    preURL.addEventListener("input", populateURL, false);
-
-                    copyFromClipboard.addEventListener("click", copyFromClipboardHandler, false);
-
-                    passwdMaster.addEventListener("change", passwdMasterHandler, false);
-                    passwdMaster.addEventListener("keyup", preGeneratePassword, false);
-                    passwdMaster.addEventListener("keydown", preGeneratePassword, false);
-                    passwdMaster.addEventListener("keypress", preGeneratePassword, false);
-                    passwdMaster.addEventListener("input", preGeneratePassword, false);
-
-                    saveMasterBtn.addEventListener("click", saveMaster, false);
-
-                    whereLeetLB.addEventListener("change", whereLeetLBHandler, false);
-
-                    leetLevelLB.addEventListener("change", leetLevelLBHandler, false);
-
-                    hashAlgorithmLB.addEventListener("change", hashAlgorithmLBHandler, false);
-                    hashAlgorithmLB.addEventListener("keyup", preGeneratePassword, false);
-                    hashAlgorithmLB.addEventListener("keydown", preGeneratePassword, false);
-                    hashAlgorithmLB.addEventListener("keypress", preGeneratePassword, false);
-                    hashAlgorithmLB.addEventListener("input", preGeneratePassword, false);
-
-                    protocolCB.addEventListener("click", protocolCBHandler, false);
-                    subdomainCB.addEventListener("click", subdomainCBHandler, false);
-                    domainCB.addEventListener("click", domainCBHandler, false);
-                    pathCB.addEventListener("click", pathCBHandler, false);
-
-                    passwdUrl.addEventListener("change", passwdUrlHandler, false);
-                    passwdUrl.addEventListener("keyup", preGeneratePassword, false);
-                    passwdUrl.addEventListener("keydown", preGeneratePassword, false);
-                    passwdUrl.addEventListener("keypress", preGeneratePassword, false);
-                    passwdUrl.addEventListener("input", preGeneratePassword, false);
-
-                    passwdLength.addEventListener("change", passwdLengthHandler, false);
-                    passwdLength.addEventListener("keyup", passwdLengthHandler, false);
-                    passwdLength.addEventListener("keydown", passwdLengthHandler, false);
-                    passwdLength.addEventListener("keypress", passwdLengthHandler, false);
-                    passwdLength.addEventListener("input", passwdLengthHandler, false);
-
-                    usernameTB.addEventListener("change", usernameTBHandler, false);
-                    usernameTB.addEventListener("keyup", preGeneratePassword, false);
-                    usernameTB.addEventListener("keydown", preGeneratePassword, false);
-                    usernameTB.addEventListener("keypress", preGeneratePassword, false);
-                    usernameTB.addEventListener("input", preGeneratePassword, false);
-
-                    counter.addEventListener("change", counterHandler, false);
-                    counter.addEventListener("keyup", preGeneratePassword, false);
-                    counter.addEventListener("keydown", preGeneratePassword, false);
-                    counter.addEventListener("keypress", preGeneratePassword, false);
-                    counter.addEventListener("input", preGeneratePassword, false);
-
-                    charset.addEventListener("change", charsetHandler, false);
-
-                    passwordPrefix.addEventListener("change", passwordPrefixHandler, false);
-                    passwordPrefix.addEventListener("keyup", preGeneratePassword, false);
-                    passwordPrefix.addEventListener("keydown", preGeneratePassword, false);
-                    passwordPrefix.addEventListener("keypress", preGeneratePassword, false);
-                    passwordPrefix.addEventListener("input", preGeneratePassword, false);
-
-                    passwordSuffix.addEventListener("change", passwordSuffixHandler, false);
-                    passwordSuffix.addEventListener("keyup", preGeneratePassword, false);
-                    passwordSuffix.addEventListener("keydown", preGeneratePassword, false);
-                    passwordSuffix.addEventListener("keypress", preGeneratePassword, false);
-                    passwordSuffix.addEventListener("input", preGeneratePassword, false);
-
-                    ifHidePasswd.addEventListener("change", ifHidePasswdHandler, false);
-
-                    copyToClipboardButton.addEventListener("click", copyToClipboardHandler, false);
-
-                    // Populate Settings pane and tie commands to Settings flyouts.
-                    WinJS.Application.onsettings = function (e) {
-                        e.detail.applicationcommands = {
-                            "helpDiv": { href: "HelpUI.html", title: "Help" },
-                            "licenseDiv": { href: "license.html", title: "License" },
-                            "pwmLibLicenseDiv": { href: "pwmLibLicense.html", title: "LGPL v2.1" },
-                            "aboutDiv": {href: "about.html", title: "About"}
-                        };
-                        WinJS.UI.SettingsFlyout.populateSettings(e);
-                    }
-                }
+                completedSetPromise()
             ));
+        } else if (args.detail.kind === activation.ActivationKind.shareTarget) {
+            args.setPromise(WinJS.UI.processAll().then(
+                completedSetPromise()
+            ));
+
+            shareOperation = args.detail.shareOperation;
+
+            // We queue an asychronous event so that working with the ShareOperation object does not 
+            // block or delay the return of the activation handler. 
+            WinJS.Application.queueEvent({ type: "shareready" });
         }
+        
     };
+
+    function completedSetPromise() {
+
+        preURL.addEventListener("change", preURLHandler, false);
+        preURL.addEventListener("keyup", populateURL, false);
+        preURL.addEventListener("keydown", populateURL, false);
+        preURL.addEventListener("keypress", populateURL, false);
+        preURL.addEventListener("input", populateURL, false);
+
+        copyFromClipboard.addEventListener("click", copyFromClipboardHandler, false);
+
+        passwdMaster.addEventListener("change", passwdMasterHandler, false);
+        passwdMaster.addEventListener("keyup", preGeneratePassword, false);
+        passwdMaster.addEventListener("keydown", preGeneratePassword, false);
+        passwdMaster.addEventListener("keypress", preGeneratePassword, false);
+        passwdMaster.addEventListener("input", preGeneratePassword, false);
+
+        saveMasterBtn.addEventListener("click", saveMaster, false);
+
+        whereLeetLB.addEventListener("change", whereLeetLBHandler, false);
+
+        leetLevelLB.addEventListener("change", leetLevelLBHandler, false);
+
+        hashAlgorithmLB.addEventListener("change", hashAlgorithmLBHandler, false);
+        hashAlgorithmLB.addEventListener("keyup", preGeneratePassword, false);
+        hashAlgorithmLB.addEventListener("keydown", preGeneratePassword, false);
+        hashAlgorithmLB.addEventListener("keypress", preGeneratePassword, false);
+        hashAlgorithmLB.addEventListener("input", preGeneratePassword, false);
+
+        protocolCB.addEventListener("click", protocolCBHandler, false);
+        subdomainCB.addEventListener("click", subdomainCBHandler, false);
+        domainCB.addEventListener("click", domainCBHandler, false);
+        pathCB.addEventListener("click", pathCBHandler, false);
+
+        passwdUrl.addEventListener("change", passwdUrlHandler, false);
+        passwdUrl.addEventListener("keyup", preGeneratePassword, false);
+        passwdUrl.addEventListener("keydown", preGeneratePassword, false);
+        passwdUrl.addEventListener("keypress", preGeneratePassword, false);
+        passwdUrl.addEventListener("input", preGeneratePassword, false);
+
+        passwdLength.addEventListener("change", passwdLengthHandler, false);
+        passwdLength.addEventListener("keyup", passwdLengthHandler, false);
+        passwdLength.addEventListener("keydown", passwdLengthHandler, false);
+        passwdLength.addEventListener("keypress", passwdLengthHandler, false);
+        passwdLength.addEventListener("input", passwdLengthHandler, false);
+
+        usernameTB.addEventListener("change", usernameTBHandler, false);
+        usernameTB.addEventListener("keyup", preGeneratePassword, false);
+        usernameTB.addEventListener("keydown", preGeneratePassword, false);
+        usernameTB.addEventListener("keypress", preGeneratePassword, false);
+        usernameTB.addEventListener("input", preGeneratePassword, false);
+
+        counter.addEventListener("change", counterHandler, false);
+        counter.addEventListener("keyup", preGeneratePassword, false);
+        counter.addEventListener("keydown", preGeneratePassword, false);
+        counter.addEventListener("keypress", preGeneratePassword, false);
+        counter.addEventListener("input", preGeneratePassword, false);
+
+        charset.addEventListener("change", charsetHandler, false);
+
+        passwordPrefix.addEventListener("change", passwordPrefixHandler, false);
+        passwordPrefix.addEventListener("keyup", preGeneratePassword, false);
+        passwordPrefix.addEventListener("keydown", preGeneratePassword, false);
+        passwordPrefix.addEventListener("keypress", preGeneratePassword, false);
+        passwordPrefix.addEventListener("input", preGeneratePassword, false);
+
+        passwordSuffix.addEventListener("change", passwordSuffixHandler, false);
+        passwordSuffix.addEventListener("keyup", preGeneratePassword, false);
+        passwordSuffix.addEventListener("keydown", preGeneratePassword, false);
+        passwordSuffix.addEventListener("keypress", preGeneratePassword, false);
+        passwordSuffix.addEventListener("input", preGeneratePassword, false);
+
+        ifHidePasswd.addEventListener("change", ifHidePasswdHandler, false);
+
+        copyToClipboardButton.addEventListener("click", copyToClipboardHandler, false);
+
+        // Populate Settings pane and tie commands to Settings flyouts.
+        WinJS.Application.onsettings = function (e) {
+            e.detail.applicationcommands = {
+                "helpDiv": { href: "HelpUI.html", title: "Help" },
+                "licenseDiv": { href: "license.html", title: "License" },
+                "pwmLibLicenseDiv": { href: "pwmLibLicense.html", title: "LGPL v2.1" },
+                "aboutDiv": { href: "about.html", title: "About" }
+            };
+            WinJS.UI.SettingsFlyout.populateSettings(e);
+        }
+    }
 
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
@@ -344,5 +371,39 @@
         WinJS.Application.sessionState.passwordSuffix = passwordSuffix.value;
     }
 
+    /// <summary> 
+    /// Handler executed when ready to share; handling the share operation should be performed 
+    /// outside the activation handler 
+    /// </summary> 
+    function shareReady(eventArgs) {
+        var sharedData = shareOperation.data;
+
+        if (sharedData.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
+            // Set the preURL to the shared data
+            sharedData.getTextAsync().done(function (text) {
+                preURL.innerText = text;
+                populateURL();
+                WinJS.Application.sessionState.preURL = preUrl.value;
+                WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+
+            }, function (e) {
+                preURL.innerText = "Error Receiving Data from Share";
+            });
+        }
+        if (sharedData.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.webLink)) {
+            sharedData.getWebLinkAsync().done(function (webLink) {
+                preURL.innerText = webLink.rawUri;
+                populateURL();
+                WinJS.Application.sessionState.preURL = preUrl.value;
+                WinJS.Application.sessionState.passwdUrl = passwdUrl.value;
+            }, function (e) {
+                preURL.innerText = "Error Receiving Data from Share";
+            });
+        }
+
+    }
+
+
+    WinJS.Application.addEventListener("shareready", shareReady, false);
     app.start();
 })();
