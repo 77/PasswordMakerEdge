d6fad732b4d314c3264583f3b20058d6f068e42a
gpbenton <gpbenton@live.com>
2015-03-13 15:19:24 +0000

    Save Hide password settings in roamingSettings

--- PasswordMaker/js/default.js
+++ PasswordMaker/js/default.js
@@ -134,6 +134,8 @@
         passwordSuffix.addEventListener("keypress", preGeneratePassword, false);
         passwordSuffix.addEventListener("input", preGeneratePassword, false);
 
+        ifHidePasswd.checked = Windows.Storage.ApplicationData.current.roamingSettings.values["displayGenerated"] ? true : false;
+        ifHidePasswdHandler();
         ifHidePasswd.addEventListener("change", ifHidePasswdHandler, false);
 
         copyToClipboardButton.addEventListener("click", copyToClipboardHandler, false);
@@ -208,7 +210,7 @@
 
     function ifHidePasswdHandler(eventInfo) {
         if (ifHidePasswd.checked == true) {
-            passwdGenerated.style.color = '#fff';
+            passwdGenerated.style.color = "transparent";
         } else {
             passwdGenerated.style.color = '#00f';
         }
@@ -802,5 +804,34 @@
 
     WinJS.Namespace.define("ProfileData", publicMembers);
 
+    // From http://brandonlive.com/2013/06/06/winjs-helper-appsettingtoggle-bind-a-toggleswitch-to-an-appdata-settings-value
+    WinJS.Namespace.define("BrandonUtils.Controls", {
+        // Options:
+        // settingName - Required. The name of the setting to toggle.
+        // getSettingContainer - Function to return setting container to use.
+        // enableRoaming - Use roaming setting container.
+        // If neither of the above are provided, the default local container is used.
+        AppSettingToggle: WinJS.Class.derive(WinJS.UI.ToggleSwitch, function (element, options) {
+            WinJS.UI.ToggleSwitch.apply(this, [element, options]);
+            WinJS.Utilities.addClass(element, "AppSettingToggle");
+            if (options.getSettingContainer) {
+                this.settingContainer = options.getSettingContainer();
+            }
+            else if (options.enableRoaming) {
+                this.settingContainer = Windows.Storage.ApplicationData.current.roamingSettings.values;
+            }
+            else {
+                this.settingContainer = Windows.Storage.ApplicationData.current.localSettings.values;
+            }
+
+            this.checked = this.settingContainer.hasKey(options.settingName) && this.settingContainer[options.settingName];
+            this.onchange = this._onChange.bind(this);
+        },
+        {
+            _onChange: function () {
+                this.settingContainer.insert(this.settingName, this.checked);
+            },
+        }),
+    });
     app.start();
 })();
